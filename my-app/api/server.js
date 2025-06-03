194ac884e60a3aa0c334348fee5c2badb405d608
yamadamasashi <shisama94@gmail.com>
2025-06-04 01:18:25 +0900

    prismaでユーザー登録apiを作成中

--- my-app/api/server.js
+++ my-app/api/server.js
@@ -1,3 +1,4 @@
+const { PrismaClient } = require('./generated/prisma');
 const express = require('express');
 const app = express();
 const PORT = 5001; //  5000だと[nodemon] clean exit - waiting for changes before restart
@@ -7,6 +8,21 @@ const PORT = 5001; //  5000だと[nodemon] clean exit - waiting for changes befo
 // });
 
 // https://www.prisma.io/docs/getting-started/quickstart-sqlite
+
+const prisma = new PrismaClient();
+
+// 新規ユーザー登録API
+app.post('/api/auth/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+  return res.json({ user });
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on Port ${PORT}`);
