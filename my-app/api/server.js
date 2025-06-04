fac7bec18d7c657bbc5d112a113435626427f189
yamadamasashi <shisama94@gmail.com>
2025-06-04 22:51:23 +0900

    thunder clientというVScodeアプリを使用

--- my-app/api/server.js
+++ my-app/api/server.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('./generated/prisma');
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 5001; //  5000だと[nodemon] clean exit - waiting for changes before restart
 
@@ -11,14 +12,22 @@ const PORT = 5001; //  5000だと[nodemon] clean exit - waiting for changes befo
 
 const prisma = new PrismaClient();
 
+app.use(express.json()); // express側に、json形式で送ると宣言しておく
+
 // 新規ユーザー登録API
+// postできるかのテストとして、thunder clientというVScodeアプリを使用した。 post先はhttp://localhost:5001/api/auth/register
 app.post('/api/auth/register', async (req, res) => {
   const { username, email, password } = req.body;
+
+  const hashPassWord = await bcrypt.hash(password, 10); // hash化できる
+
+  console.log(hashPassWord);
+
   const user = await prisma.user.create({
     data: {
       username,
       email,
-      password,
+      password: hashPassWord,
     },
   });
   return res.json({ user });
