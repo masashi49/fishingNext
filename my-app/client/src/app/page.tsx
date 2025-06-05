27ab24215907586597ec0d0842f2f94eae068775
yamadamasashi <shisama94@gmail.com>
2025-06-05 23:51:02 +0900

    tailwindcssをしようsルウ

--- my-app/client/src/app/page.tsx
+++ my-app/client/src/app/page.tsx
@@ -3,16 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        hello next.js
-      </main>
-      <div></div>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-<div>
-  あういさrd
-</div>
-<p>asdf</p>
-      </footer>
+      <h1 className="text-3xl font-bold underline">こんにちわ</h1>
     </div>
   );
 }
